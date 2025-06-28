a848307c0ee554c8b09f0127bddf90d7422f2528
jraczynski <raczynski.joseph616@gmail.com$ git config --global user.email raczynski.joseph616@gmail.com>
2025-06-28 12:44:18 -0500

    change variables a,b, c, and d to more descriptive names

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,9 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let name = "Alice";
+let amount = 5;
+let dollar = 20;
+let GroceryOrder = name + " bought " + amount + " items for $" + dollar + ".";
 
-console.log(d);
+console.log(GroceryOrder);
